911b4c258780b9f53c9b55b8438466e478aeca20
Alik S <alik.sayf@gmail.com>
2020-06-20 20:59:34 -0700

    query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import axios from "axios";
+
+const getAll = 'http://localhost:5000/todo'
 
 function App() {
+    const [todoList, setTodoList] = useState([])
+
+    const getTodo = () => {
+        axios({
+            method: 'get',
+            url: getAll,
+        })
+            .then(function (response) {
+                console.log(response.data)
+            });
+    }
+
+    useEffect(()=>{
+        getTodo();
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoForm/>
+      <TodoList/>
     </div>
   );
 }
---
+++ src/TodoForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function TodoForm() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default TodoForm;
