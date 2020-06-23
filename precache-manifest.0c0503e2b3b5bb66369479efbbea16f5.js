bc6c61992f0e31aae7a3ae05ce8103e99fb08437
Alik S <alik.sayf@gmail.com>
2020-06-22 21:18:59 -0700

    Updates

---
+++ precache-manifest.0c0503e2b3b5bb66369479efbbea16f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ef4fd553bacb951e48f74e8d2ea909",
+    "url": "/todo-client/index.html"
+  },
+  {
+    "revision": "048e9b205405abd04d2e",
+    "url": "/todo-client/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bc617a946671ba91ca9f",
+    "url": "/todo-client/static/css/main.eea13354.chunk.css"
+  },
+  {
+    "revision": "048e9b205405abd04d2e",
+    "url": "/todo-client/static/js/2.df3d0322.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-client/static/js/2.df3d0322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc617a946671ba91ca9f",
+    "url": "/todo-client/static/js/main.472e960a.chunk.js"
+  },
+  {
+    "revision": "115c2f20bf694a169590",
+    "url": "/todo-client/static/js/runtime-main.ffdc7b88.js"
+  }
+]);
